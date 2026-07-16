e2f494af46c183151f6fab221bff58dfbf44f290
nezerock <bermartin888@gmail.com>
2026-07-16 18:28:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,28 @@ if not BOT_TOKEN:
     sys.exit(1)
 
 # Настройки игры
-DAILY_BONUS = 100
+DAILY_BONUS = 5
 START_BALANCE = 50
 MIN_BET = 1
 MAX_BET = 1000
 
+# Множители выигрышей
+SLOT_MULTIPLIER = 20  # 20x
+DICE_MULTIPLIER = 1.7  # 1.7x
+
 # Кликер
 CLICK_REWARD = 0.1
-CLICK_COOLDOWN = 5  # секунд
+CLICK_COOLDOWN = 5
 MAX_CLICKS_PER_DAY = 50
 
-# Эмодзи для игр
+# Магазин бонусов
+SHOP_ITEMS = {
+    '1.3x': {'price': 25, 'multiplier': 1.3},
+    '1.5x': {'price': 40, 'multiplier': 1.5},
+    '2.0x': {'price': 70, 'multiplier': 2.0}
+}
+
+# Эмодзи
 EMOJI = {
     'dice': '🎲',
     'coin': '🪙',
@@ -29,5 +40,6 @@ EMOJI = {
     'diamond': '💎',
     'click': '👆',
     'games': '🎮',
-    'leaderboard': '📊'
+    'leaderboard': '📊',
+    'shop': '🏪'
 }
